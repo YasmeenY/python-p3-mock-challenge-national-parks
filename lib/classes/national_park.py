c60188d44da2fad810110ee620681bc6d37a8dcd
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-05-11 10:48:28 -0400

    done

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -5,16 +5,37 @@ class NationalPark:
         self._trips = []
         self._visitors = []
         
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if type(name) == str and not hasattr(self, "name"):
+            self._name = name
+        else:
+            raise Exception("Invalid operation")
+    
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        if isinstance(new_visitor, Visitor) and new_visitor not in self._visitors:
+            self._visitors.append(new_visitor)
+        return self._visitors
     
     def total_visits(self):
-        pass
+        return len(self._trips)
     
     def best_visitor(self):
-        pass
+        max_num = 0
+        best_vis = None
+        for visitor in self._visitors:
+            visits = len([trip for trip in self._trips if trip.visitor == visitor])
+            if visits > max_num :
+                max_num = visits
+                best_vis = visitor
+            return best_vis
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -2,11 +2,26 @@ class Visitor:
 
     def __init__(self, name):
         self.name = name
-        
+        self._trips = []
+        self._national_park = []
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if type(name) == str and 1 <= len(name) <= 15 and not hasattr (self, "name"):
+            self._name = name
+        else:
+            raise Exception("Invalid name")
+    
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
-        pass
+        if isinstance(new_national_park, NationalPark) and new_national_park not in self._national_park:
+            self._national_park.append(new_national_park)
+        return self._national_park
